2a5d94319e586b86cf6e419a74510ed25dc9665d
Andrew Chen <andrew@smartovation.com>
2011-03-07 11:30:56 -0800

    Remove unnecessary NSString category that was causing problems
    Signed-off-by: Andrew Chen <andrew@smartovation.com>
    
    Signed-off-by: Sixten Otto <sixten@resultsdirect.com>

--- LinkedInClientLibrary/RDLinkedInHTTPURLConnection.m
+++ LinkedInClientLibrary/RDLinkedInHTTPURLConnection.m
@@ -7,7 +7,6 @@
 //
 
 #import "RDLinkedInHTTPURLConnection.h"
-#import "NSString+UUID.h"
 
 
 @implementation RDLinkedInHTTPURLConnection
@@ -31,7 +30,7 @@
 
 - (RDLinkedInConnectionID *)identifier {
   if( !rdIdentifier ) {
-    rdIdentifier = [[NSString stringWithNewUUID] retain];
+    rdIdentifier = [[[NSProcessInfo processInfo] globallyUniqueString] retain];
   }
   return rdIdentifier;
 }
